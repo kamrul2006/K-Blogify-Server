6889b9d4cad89ff6b13349417e82bf419dd26512
k-web-tech <kamrulislamapurba@gmail.com>
2025-06-14 20:21:04 +0600

    Completed

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ app.use(cors(
     {
         origin: ['http://localhost:3000',
             'http://localhost:3000',
-            // 'https://k-infonic.web.app',
-            // 'https://k-infonic.firebaseapp.com'
+
         ],
         methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization'],
@@ -46,9 +45,6 @@ async function run() {
         const PostsCollection = client.db("kblogify").collection('posts')
 
 
-
-
-
         // ----------------------------------------------------------------------------------------
         //------all POSTS ---------
         // ----------------------------------------------------------------------------------------
@@ -64,7 +60,7 @@ async function run() {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
 
-            const result = await POSTSCollection.findOne(query)
+            const result = await PostsCollection.findOne(query)
             res.send(result)
         })
 
@@ -81,7 +77,7 @@ async function run() {
         app.delete("/POSTS/:id", async (req, res) => {
             const { id } = req.params;
             try {
-                const result = await POSTSCollection.deleteOne({ _id: new ObjectId(id) });
+                const result = await PostsCollection.deleteOne({ _id: new ObjectId(id) });
                 if (result.deletedCount === 1) {
                     res.status(200).json({ message: "POSTS deleted successfully." });
                 } else {
@@ -93,34 +89,6 @@ async function run() {
             }
         });
 
-        // ------------- approve POSTS---------------
-        app.patch('/POSTS/approve/:id', async (req, res) => {
-            const id = req.params.id;
-            try {
-                const result = await POSTSCollection.updateOne(
-                    { _id: new ObjectId(id) },
-                    { $set: { status: 'approved' } }
-                );
-                res.send(result);
-            } catch (error) {
-                res.status(500).send({ message: 'Failed to approve POSTS' });
-            }
-        });
-
-
-        // ----------------- make POSTS general-------------
-        app.patch('/POSTS/general/:id', async (req, res) => {
-            const id = req.params.id;
-            try {
-                const result = await POSTSCollection.updateOne(
-                    { _id: new ObjectId(id) },
-                    { $set: { type: 'general', isPremium: false } }
-                );
-                res.send(result);
-            } catch (error) {
-                res.status(500).send({ message: 'Failed to make general' });
-            }
-        });
 
 
 
@@ -129,93 +97,93 @@ async function run() {
         // ----------------------------------------------------------------------------------------
 
         // -----------get all user---------------------------------
-        app.get("/Users", async (req, res) => {
-            const result = await UserCollection.find().toArray();
-            res.send(result)
-        })
+        // app.get("/Users", async (req, res) => {
+        //     const result = await UserCollection.find().toArray();
+        //     res.send(result)
+        // })
 
         //---------------------------------add users-------------------
-        app.post('/Users', async (req, res) => {
-            const user = req.body
-            const query = { email: user.email }
-            const exist = await UserCollection.findOne(query)
+        // app.post('/Users', async (req, res) => {
+        //     const user = req.body
+        //     const query = { email: user.email }
+        //     const exist = await UserCollection.findOne(query)
 
-            if (exist) {
-                return res.send({ massage: 'User Already exist', insertedId: null })
-            }
+        //     if (exist) {
+        //         return res.send({ massage: 'User Already exist', insertedId: null })
+        //     }
 
-            const result = await UserCollection.insertOne(user)
-            res.send(result)
-        })
+        //     const result = await UserCollection.insertOne(user)
+        //     res.send(result)
+        // })
 
         // --------------------- update isSubscribed------------
-        app.patch("/Users/:id", async (req, res) => {
-            const userId = req.params.id;
-            const { isSubscribed } = req.body;
+        // app.patch("/Users/:id", async (req, res) => {
+        //     const userId = req.params.id;
+        //     const { isSubscribed } = req.body;
 
-            try {
-                const result = await UserCollection.updateOne(
-                    { _id: new ObjectId(userId) },
-                    { $set: { isSubscribed: isSubscribed } }
-                );
+        //     try {
+        //         const result = await UserCollection.updateOne(
+        //             { _id: new ObjectId(userId) },
+        //             { $set: { isSubscribed: isSubscribed } }
+        //         );
 
-                if (result.modifiedCount > 0) {
-                    res.send({ success: true, message: "Subscription updated." });
-                } else {
-                    res.status(404).send({ success: false, message: "User not found or already updated." });
-                }
-            } catch (error) {
-                res.status(500).send({ success: false, message: "Update failed.", error });
-            }
-        });
+        //         if (result.modifiedCount > 0) {
+        //             res.send({ success: true, message: "Subscription updated." });
+        //         } else {
+        //             res.status(404).send({ success: false, message: "User not found or already updated." });
+        //         }
+        //     } catch (error) {
+        //         res.status(500).send({ success: false, message: "Update failed.", error });
+        //     }
+        // });
 
         // ---------------------- remove admin ---------------------
-        app.patch("/Users/remove-admin/:id", async (req, res) => {
-            const userId = req.params.id;
+        // app.patch("/Users/remove-admin/:id", async (req, res) => {
+        //     const userId = req.params.id;
 
-            // console.log('xxgjh')
+        //     // console.log('xxgjh')
 
-            try {
-                const result = await UserCollection.updateOne(
-                    { _id: new ObjectId(userId) },
-                    { $set: { role: "user" } }
-                );
+        //     try {
+        //         const result = await UserCollection.updateOne(
+        //             { _id: new ObjectId(userId) },
+        //             { $set: { role: "user" } }
+        //         );
 
-                res.json(result);
-            } catch (err) {
-                res.status(500).json({ message: "Failed to update user role", error: err.message });
-            }
-        });
+        //         res.json(result);
+        //     } catch (err) {
+        //         res.status(500).json({ message: "Failed to update user role", error: err.message });
+        //     }
+        // });
 
         // ---------------------- make admin---------------------
-        app.patch("/Users/admin/:id", async (req, res) => {
-            const userId = req.params.id;
+        // app.patch("/Users/admin/:id", async (req, res) => {
+        //     const userId = req.params.id;
 
-            // console.log('xxgjh')
+        //     // console.log('xxgjh')
 
-            try {
-                const result = await UserCollection.updateOne(
-                    { _id: new ObjectId(userId) },
-                    { $set: { role: "admin" } }
-                );
+        //     try {
+        //         const result = await UserCollection.updateOne(
+        //             { _id: new ObjectId(userId) },
+        //             { $set: { role: "admin" } }
+        //         );
 
-                res.json(result);
-            } catch (err) {
-                res.status(500).json({ message: "Failed to update user role", error: err.message });
-            }
-        });
+        //         res.json(result);
+        //     } catch (err) {
+        //         res.status(500).json({ message: "Failed to update user role", error: err.message });
+        //     }
+        // });
 
         // //----------------- DELETE a user---------------
-        app.delete("/Users/:id", async (req, res) => {
-            const userId = req.params.id;
+        // app.delete("/Users/:id", async (req, res) => {
+        //     const userId = req.params.id;
 
-            try {
-                const result = await UserCollection.deleteOne({ _id: new ObjectId(userId) });
-                res.json(result);
-            } catch (err) {
-                res.status(500).json({ message: "Failed to delete user", error: err.message });
-            }
-        });
+        //     try {
+        //         const result = await UserCollection.deleteOne({ _id: new ObjectId(userId) });
+        //         res.json(result);
+        //     } catch (err) {
+        //         res.status(500).json({ message: "Failed to delete user", error: err.message });
+        //     }
+        // });
 
 
         //--------------------------------------------------------------------
@@ -223,33 +191,33 @@ async function run() {
         //---------------------------------------------------------------------
 
         // ---------------get all Review----------------
-        app.get("/reviews", async (req, res) => {
-            const result = await ReviewsCollection.find().toArray();
-            res.send(result)
-        })
+        // app.get("/reviews", async (req, res) => {
+        //     const result = await ReviewsCollection.find().toArray();
+        //     res.send(result)
+        // })
 
         //----------add review--------
-        app.post('/reviews', async (req, res) => {
-            const review = req.body
-            const result = await ReviewsCollection.insertOne(review)
-            res.send(result)
-        })
+        // app.post('/reviews', async (req, res) => {
+        //     const review = req.body
+        //     const result = await ReviewsCollection.insertOne(review)
+        //     res.send(result)
+        // })
 
         //---------- DELETE review by id----------------
-        app.delete("/Reviews/:id", async (req, res) => {
-            const { id } = req.params;
-            try {
-                const result = await ReviewsCollection.deleteOne({ _id: new ObjectId(id) });
-                if (result.deletedCount === 1) {
-                    res.status(200).json({ message: "Review deleted successfully." });
-                } else {
-                    res.status(404).json({ message: "Review not found." });
-                }
-            } catch (err) {
-                console.error("Delete Error:", err);
-                res.status(500).json({ message: "Server error." });
-            }
-        });
+        // app.delete("/Reviews/:id", async (req, res) => {
+        //     const { id } = req.params;
+        //     try {
+        //         const result = await ReviewsCollection.deleteOne({ _id: new ObjectId(id) });
+        //         if (result.deletedCount === 1) {
+        //             res.status(200).json({ message: "Review deleted successfully." });
+        //         } else {
+        //             res.status(404).json({ message: "Review not found." });
+        //         }
+        //     } catch (err) {
+        //         console.error("Delete Error:", err);
+        //         res.status(500).json({ message: "Server error." });
+        //     }
+        // });
 
     }
     finally {
